acbf58049c6cf80904375e7065f8b896c42543f1
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-13 15:44:43 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,4 @@
+numero1 = int(input("Digite o primeiro número: ")
+numero2 = int(input("Digite o segundo número: ")
+soma = numero1 + numero2
+print("A soma entre \033[4;31m{}\033[m e \033[4;34m{}\033[m é \033[4;36m{}\033[m!".format(numero1, numero2, soma)
