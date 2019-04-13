77b5840c4a61ca544c3f7abb78c121735248cdec
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-13 15:47:51 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
-numero1 = int(input("Digite o primeiro número: ")
-numero2 = int(input("Digite o segundo número: ")
+numero1 = int(input("\033[31mDigite o primeiro número: ")
+numero2 = int(input("\033[34mDigite o segundo número: \033[m")
 soma = numero1 + numero2
-print("A soma entre \033[4;31m{}\033[m e \033[4;34m{}\033[m é \033[4;36m{}\033[m!".format(numero1, numero2, soma)
+print("A soma entre \033[4;31m{}\033[m e \033[4;34m{}\033[m é \033[4;36m{}\033[m!".format(numero1, numero2, soma))
