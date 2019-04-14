b9591bb8fa3aa63d6390309b0416495210424ace
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-14 16:27:14 -0300

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -1,4 +1,4 @@
-celcius = float(input('Digite a temperaturazinha em Celsius: '))
+celcius = float(input('Digite a temperatura em Celsius: '))
 fahrenheit = (celcius*1.8)+32
 print('{}º Celsius equivalem a {}º Fahrenheit'.format(celsius,fahrenheit))
 
