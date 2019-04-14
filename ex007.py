39997025c09c7e01cb2c716ee2855a76b4d399e1
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-14 16:03:51 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,4 @@
+nota1 = float(input("\033[32mDigite a sua primeira nota: "))
+nota2 = float(input("\033[32mDigite a sua segunda nota: "))
+
+print("\033[32mA média aritmética entre \033[31m{}\033[32m e \033[31m{} \033[32mé \033[32m{}".format(nota1,nota2,(nota1+nota2)/2))
