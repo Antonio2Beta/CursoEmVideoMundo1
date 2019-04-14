a4f16546bc8ef81cbd7347aa7ec64bbe74ae6959
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-14 16:30:23 -0300

    Create ex016.py

---
+++ ex016.py
@@ -0,0 +1,4 @@
+
+num = float(input('\033[1;34mDigite um número real(ponto flutuante): '))
+print("\033[1;32mO número digitado é \033[1;34m{} \033[1;32m\nSua"
+      " parte inteira é: \033[1;35m{}".format(num, int(num)))
