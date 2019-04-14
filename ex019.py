c17b00a575cc30f8a334a3c12b25281d08dcf589
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-14 16:42:18 -0300

    Delete ex019.py

--- ex019.py
+++
@@ -1,6 +0,0 @@
-import math
-ang = float(input('\033[1;31mDigite o valor de um angulo: '))
-print('\033[1;34mO SENO de \033[1;31m{} \033[1;34mé {:.2f}\n'
-      '\033[1;34mO COSSENO de \033[1;31m{} \033[1;34mé {:.2f}\n'
-      '\033[1;34mA TANGENTE de \033[1;31m{} \033[1;34mé {:.2f}'.format(ang, math.sin(math.radians(ang)), ang, math.cos(math.radians(ang)), ang, math.tan(math.radians(ang))))
-
