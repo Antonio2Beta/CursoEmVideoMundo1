680c26edbf316e38e394bfd035eef70ded7af448
Antonio VSF <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-14 16:43:02 -0300

    Update ex018.py

--- ex018.py
+++ ex018.py
@@ -1,8 +1,6 @@
-from math import hypot
-from time import sleep
-catetoOposto = float(input('\033[1;35mDigite o valor do cateto oposto: '))
-catetoAdjacente = float(input('Digite o valor do cateto adjacente: '))
-print('\033[4;31mPROCESSANDO CÁLCULO DA HIPOTENUSA')
-sleep(3)
-hipotenusa = hypot(catetoOposto,catetoAdjacente)
-print('\033[1;34mA \033[4;31mhipotenusa \033[1;34mé \033[1;31m{:.2f}'.format(hipotenusa))
+import math
+ang = float(input('\033[1;31mDigite o valor de um angulo: '))
+print('\033[1;34mO SENO de \033[1;31m{} \033[1;34mé {:.2f}\n'
+      '\033[1;34mO COSSENO de \033[1;31m{} \033[1;34mé {:.2f}\n'
+      '\033[1;34mA TANGENTE de \033[1;31m{} \033[1;34mé {:.2f}'.format(ang, math.sin(math.radians(ang)), ang, math.cos(math.radians(ang)), ang, math.tan(math.radians(ang))))
+
